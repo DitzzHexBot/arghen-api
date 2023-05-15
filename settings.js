20385040115b23f54a9204498545c5c88f7b8dc4
arghahos <109324437+DitzzHexBot@users.noreply.github.com>
2023-05-15 16:39:40 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,7 +1,7 @@
 const fs = require('fs')
 
 global.creator = 'ArghenDev'// yourname
-global.MONGO_DB_URI = "mongodb+srv://aditbagor866:<DgNT6AsVAVXhgmyb>@cluster0.t0xocri.mongodb.net/?retryWrites=true&w=majority" //database mongodb 
+global.MONGO_DB_URI = "mongodb+srv://aditbagor866:DgNT6AsVAVXhgmyb@cluster0.t0xocri.mongodb.net/?retryWrites=true&w=majority" //database mongodb 
 global.ACTIVATION_TOKEN_SECRET = "ArghenDevBotz" //isi apa aja bebas
 global.your_email = "argatriaditya84@gmail.com" //email
 global.email_password = "kauxgngzctjzrdsg" //application password email
